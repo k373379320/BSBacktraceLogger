9f2f34e39e2446e8fdaf81d878d627c5a9268b2d
肖志斌 <373379320@qq.com>
2019-04-30 21:25:34 +0800

    fix:添加podspec;修改dladdr

--- ZBPushToGitHub.sh
+++ ZBPushToGitHub.sh
@@ -4,7 +4,7 @@ echo "pod update..."
 
 git add .
 git commit  -m "fix:添加podspec;修改dladdr"
-git tag "0.0.1"
+git tag "0.0.2"
 git push origin master
 git push --tags
 pod spec lint --verbose ZBBSBacktraceLogger.podspec --allow-warnings
